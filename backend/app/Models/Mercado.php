27dca4a676aab412a170c11c80eeae9c6d52f6d5
Rafa <rafa@aetherdocks.xyz>
2024-03-25 19:52:23 +0100

    añadido 'precio' a tabla mercado, implementado parcialmente API, necesita terminar metodos POST/PUT y PATCH, necesita testeo

---
+++ backend/app/Models/Mercado.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory; // esta linea se agrega para que funcione el modelo
+
+class Mercado extends Model{
+    use HasFactory;
+
+    /**
+     * campos de la tabla usuarios que se pueden rellenar
+     * 
+     * la variable es protegida y se llama fillable, es un array con los campos permitidos
+     *  */ 
+    
+    protected $fillable = [
+        'id_coleccion', 
+        'vendida',
+        'precio'
+    ];
+}
+
+?>
