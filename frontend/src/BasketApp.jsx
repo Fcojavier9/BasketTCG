8ff64a81ad2c91354aaae4212624dcd3007b5d75
PabloNarVal <pablonavval@gmail.com>
2024-04-18 20:44:37 +0200

    pagina de inicio

--- frontend/src/BasketApp.jsx
+++ frontend/src/BasketApp.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { InicioPage } from "./pages/InicioPage";
+import { Inicio } from "./components/Inicio";
 import { Footer } from "./components/Footer";
 import './basketApp.css';
 
@@ -11,6 +12,7 @@ export const BasketApp = () => {
       <div className="container">
         <Routes>
           <Route path="/usuarios" element={<InicioPage endPoint="usuarios" />}></Route>
+          <Route path="/" element={<Inicio endPoint="fran/coleccion" />}></Route>
           <Route path="/*" element={<Navigate to="/" />}></Route>
         </Routes>
       </div>
