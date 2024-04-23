89fdcf1b15fe9089fabcc07e00e79d484faae56d
PabloNarVal <pablonavval@gmail.com>
2024-04-23 23:23:41 +0200

    tamaños inicio ajustados

--- frontend/src/BasketApp.jsx
+++ frontend/src/BasketApp.jsx
@@ -11,7 +11,8 @@ export const BasketApp = () => {
       <NavBar/>
       <div className="container">
         <Routes>
-          <Route path="/usuarios" element={<InicioPage endPoint="usuarios" token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGktYmFza2V0LXRjZyIsInN1YiI6MSwiaWF0IjoxNzEzNjk3ODA0LCJleHAiOjE3MTM3MDE0MDR9.gd8v2lGxheM0abPe9vrlzQBZ1Xow03ZARY3ryxpoDuE" metodo="GET" />}></Route>
+        <Route path="/usuarios" element={<InicioPage endPoint="usuarios" token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGktYmFza2V0LXRjZyIsInN1YiI6MSwiaWF0IjoxNzEzNjk3ODA0LCJleHAiOjE3MTM3MDE0MDR9.gd8v2lGxheM0abPe9vrlzQBZ1Xow03ZARY3ryxpoDuE" metodo="GET" />}></Route>
+         <Route path="/usuario" element={<InicioPage />}></Route>
           <Route path="/" element={<Inicio endPoint="fran/coleccion" />}></Route>
           <Route path="/*" element={<Navigate to="/" />}></Route>
         </Routes>
--- frontend/src/components/Carta.jsx
+++ frontend/src/components/Carta.jsx
@@ -2,18 +2,17 @@
 import "../styles/cartas.css"
 
 // creamos el componente Usuarios, como no pongo default, puedo importarlo con llaves en la clase que lo llame
-export const Carta = ({id, position}) => {
+export const Carta = ({elemento, position, tipo }) => {
 
-    const imagen = id
-    const stilo = position
+    const imagen = elemento.imagen;
+    const sitio = position;
 
-    // const carta = useFetchData(endP)
-
-
-    console.log("carta",position)
-    
+    if(tipo !== undefined){
+        tipo(elemento.clase)
+    }
+             
     return (
         //le coloco el tamaño de la imagen por motivo de comodidad ya que de momento sera lo unico que le editare aqui
-        <img src={imagen} alt="carta" className={stilo}/> 
+        <img src={imagen} alt="carta" className={sitio}/> 
     )
 }
