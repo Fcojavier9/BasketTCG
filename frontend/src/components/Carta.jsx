b757e975b31b2e1f8b692dc9314f6c612ca60668
PabloNarVal <pablonavval@gmail.com>
2024-05-17 00:59:25 +0200

    feat/Coleccionv1.2 colection and init complete

--- frontend/src/components/Carta.jsx
+++ frontend/src/components/Carta.jsx
@@ -1,17 +1,34 @@
 // importamos el customHooks a utilizar
-import "../styles/cartas.css"
+import "../styles/cartas.css";
+import cartaComun from "../assets/cartaComun.png";
+import cartaRara from "../assets/cartaRara.png";
+import cartasHeroe from "../assets/cartasHeroe.png";
+import { useEffect, useState } from "react";
+import { fetchData } from "../helpers/fetchData";
 
 // creamos el componente Usuarios, como no pongo default, puedo importarlo con llaves en la clase que lo llame
-export const Carta = ({elemento, position, tipo, id}) => {
-
-    const imagen = elemento.imagen;
-    const estilo = position;
-
-   tipo && tipo(elemento.clase)
-
-             
+export const Carta = ({carta, estilo, accion}) => {
+    const [imagen, setImagen] = useState()
+    
+    // ------------- A espera de tener imagenes de cartas ----------//
+    // se eliminara o modificara entonces
+    useEffect(() => {
+        if(carta !== undefined ){
+        switch (carta?.rarity) {
+            case "heroe":
+                setImagen(cartasHeroe);
+                break;
+            case "rara":
+                setImagen(cartaRara);
+                break;
+            case "comun":
+                setImagen(cartaComun);
+                break;
+        }
+    } 
+}, [carta]);
     return (
         //le coloco el tamaño de la imagen por motivo de comodidad ya que de momento sera lo unico que le editare aqui
-        <img src={imagen} alt="carta" className={estilo}/> 
+        <img src={imagen} alt="carta" className={estilo} onClick={accion}/> 
     )
 }
