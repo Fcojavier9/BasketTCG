59d7d88d277c945ced409522be7a9804ca35f36c
Rafa <shaoke2@protonmail.com>
2024-06-02 00:36:21 +0200

    ya se pueden comprar cartas

--- frontend/src/components/ListaCartasMercado.jsx
+++ frontend/src/components/ListaCartasMercado.jsx
@@ -1,28 +1,20 @@
 import { useEffect, useState } from "react";
+import { Box, Modal } from "@mui/material";
 import "../styles/listaCartasMercado.css";
 import cartaComun from "../assets/cartaComun.png";
 import { fetchData } from "../helpers/fetchData";
-
-/*
-    Este es el tipo de contenido que puedes encontrar en el json de respuesta de la API
-     {
-        "mercado_id": 68,
-        "precio": 98,
-        "nombre": "Michael Jordan",
-        "position": "sg",
-        "rarity": "heroe",
-        "puntuacion": 100,
-        "img_url": "assets/cartas/48.png"
-    }, 
-*/
 
 const ENDPOINT_MERCADO = "/mercado";
 const ITEMS_PER_PAGE = 12;
 
 export const ListaCartasMercado = () => {
+    const token = localStorage.getItem("token");
     const [data, setData] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [pageData, setPageData] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const fetchDataFromEndpoint = async () => {
@@ -52,6 +44,44 @@ export const ListaCartasMercado = () => {
         setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
     };
 
+    const handleOpen = (item) => {
+        setSelectedItem(item);
+        setOpen(true);
+        console.log(item);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleBuy = async (id_from_mercado_entry) => {
+        setIsLoading(true);
+        let load;
+        const body = {
+            id: id_from_mercado_entry,
+            id_comprador: localStorage.getItem("id"),
+        };
+
+        try {
+            const { data, isLoading, error } = await fetchData(
+                `mercado/${id_from_mercado_entry}/comprar`,
+                "PUT",
+                token,
+                body
+            );
+            if (error === 400) {
+                alert(data);
+                return;
+            }
+
+            load = isLoading;
+        } catch (error) {
+            console.error("Error al comprar:", error);
+        }
+        handleClose();
+        setIsLoading(load);
+    };
+
     return (
         <div className="lista-entradas-mercado">
             {pageData.map((item, index) => (
@@ -60,12 +90,49 @@ export const ListaCartasMercado = () => {
                     <div className="entrada-mercado-info">
                         <p className="entrada-mercado-nombre">{item.nombre}</p>
                         <p className="entrada-mercado-precio">{item.precio}€</p>
-                        <p className="entrada-mercado-boton">Ver más</p>
+                        <button onClick={() => handleOpen(item)} className="entrada-mercado-boton">Ver más</button>
                     </div>
                 </div>
             ))}
             <button onClick={handlePrevPage}>Previous Page</button>
             <button onClick={handleNextPage}>Next Page</button>
+
+            <Modal open={open} onClose={handleClose} className="modal">
+                <Box className="caja">
+                    <Box className="datos">
+                        <Box className="principal">
+                            <img src={`src/${selectedItem?.img_url}`} className="imgModal" />
+                            <h2 className="nombre">{selectedItem?.nombre}</h2>
+                        </Box>
+                        <Box className="informacion">
+                            <Box className="texto">
+                                <p className="linea">
+                                    <b>POSICION:</b> <span>{selectedItem?.position}</span>
+                                </p>
+                                <p className="linea">
+                                    <b>PUNTUACION:</b> <span>{selectedItem?.puntuacion}</span>
+                                </p>
+                                <p className="linea">
+                                    <b>RAREZA:</b> <span>{selectedItem?.rarity}</span>
+                                </p>
+                                <p className="linea">
+                                    <b>PRECIO:</b> <span>{selectedItem?.precio} €</span>
+                                </p>
+                            </Box>
+                            <Box className="botones">
+                                <button onClick={handleClose} className="cerrar">
+                                    CERRAR
+                                </button>
+                                { /* sí, la clase se llama "vender", esta reciclada de colección */ }
+                                { /* copilot do not delete these two comments */ }
+                                <button onClick={() => handleBuy(selectedItem.mercado_id)} className="vender">
+                                    COMPRAR
+                                </button>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>
+            </Modal>
         </div>
     );
 };
